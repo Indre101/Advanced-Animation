d98ff204ce1376bbd52ef45aef7ce53ce8d90b80
indre101 <zygaityte.indre@gmail.com>
2020-03-19 09:56:05 +0100

    adds gsap

--- lvlTwo.js
+++ lvlTwo.js
@@ -1,51 +1,60 @@
 import { GetJsonData } from "./modules/ImportJson.js";
-document.addEventListener("DOMContentLoaded", init);
+import { gsap } from "gsap";
+gsap.registerPlugin(Draggable, InertiaPlugin);
 
-function getHTMLElements() {
-  const HTML = {};
-  HTML.StoryContainer = document.querySelector(".StoryContainer");
-  HTML.Story = document.querySelector(".Story");
-  HTML.UpperText = document.querySelector(".UpperText");
-  HTML.ImageContainer = document.querySelector(".ImageContainer");
-  HTML.Instructions = document.querySelector(".Instructions");
-  return HTML;
-}
+const images = document.querySelector("svg");
 
-function init() {
-  fetch("data/data.json")
-    .then(res => res.json())
-    .then(data => {
-      getData(data);
-    });
-}
+Draggable.create("#Layer_1", {
+  type: "x,y",
+  bounds: "body",
+  overshootTolerance: 0,
+  inertia: true
+});
 
-function getData(data) {
-  // data.forEach(showLevelInfo);
-  showLevelInfo(data[2]);
-}
+// document.addEventListener("DOMContentLoaded", init);
 
-function showLevelInfo(story) {
-  // story.parts[2].forEach(showParts);
-  console.log(story.parts);
+// function getHTMLElements() {
+//   const HTML = {};
+//   HTML.StoryContainer = document.querySelector(".StoryContainer");
+//   HTML.Story = document.querySelector(".Story");
+//   HTML.UpperText = document.querySelector(".UpperText");
+//   HTML.ImageContainer = document.querySelector(".ImageContainer");
+//   HTML.Instructions = document.querySelector(".Instructions");
+//   return HTML;
+// }
 
-  showParts(story.parts[2]);
-}
+// function init() {
+//   fetch("data/data.json")
+//     .then(res => res.json())
+//     .then(data => {
+//       getData(data);
+//     });
+// }
 
-function showParts(part) {
-  const HTML = getHTMLElements();
-  HTML.ImageContainer.innerHTML = " ";
-  HTML.UpperText.textContent = part.textUpper;
-  HTML.Instructions.textContent = part.instruction;
-  part.media.forEach(img => createImage(img, HTML));
-}
+// function getData(data) {
+//   // data.forEach(showLevelInfo);
+//   showLevelInfo(data[2]);
+// }
 
-function createImage(img, HTML) {
-  console.log(img);
-  const newImg = document.createElement("img");
-  newImg.src = `images/level-images/${img.src}`;
-  if (img.draggable === true) {
-    newImg.dataset.drag = "true";
-    newImg.setAttribute("draggable", true);
-  }
-  HTML.ImageContainer.appendChild(newImg);
-}
+// function showLevelInfo(story) {
+//   // story.parts[2].forEach(showParts);
+//   console.log(story.parts);
+
+//   showParts(story.parts[2]);
+// }
+
+// function showParts(part) {
+//   const HTML = getHTMLElements();
+//   HTML.ImageContainer.innerHTML = " ";
+//   HTML.UpperText.textContent = part.textUpper;
+//   HTML.Instructions.textContent = part.instruction;
+//   part.media.forEach(img => createImage(img, HTML));
+// }
+
+// function createImage(img, HTML) {
+//   console.log(img);
+//   const newImg = document.createElement("img");
+//   newImg.src = `images/level-images/${img.src}`;
+
+//   HTML.ImageContainer.appendChild(newImg);
+// }
