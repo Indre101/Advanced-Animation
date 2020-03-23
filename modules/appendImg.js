e144065cf34b823a98f1b26d3012036a8c266051
Ingimar <ingimareys93@gmail.com>
2020-03-23 14:01:27 +0100

    I'm finishing the first chapter

--- modules/appendImg.js
+++ modules/appendImg.js
@@ -1,16 +1,26 @@
 import interact from "interactjs";
-
+import { gsap } from "gsap";
 export const AppendImg = data => {
   const ImageContainer = document.querySelector(".ImageContainer");
-  ImageContainer.innerHTML = "";
-  ImageContainer.dataset.chapter = data[0].id;
-  if (data[0].media.length > 0) {
-    data[0].media.forEach(e => {
-      createSvg(e, ImageContainer);
+  gsap.to(ImageContainer, {
+    duration: 1,
+    opacity: 0
+  });
+  setTimeout(() => {
+    ImageContainer.innerHTML = "";
+    gsap.to(ImageContainer, {
+      duration: 1,
+      opacity: 1
     });
-  } else {
-    createSvg(data[0].media[0], ImageContainer);
-  }
+    ImageContainer.dataset.chapter = data[0].id;
+    if (data[0].media.length > 0) {
+      data[0].media.forEach(e => {
+        createSvg(e, ImageContainer);
+      });
+    } else {
+      createSvg(data[0].media[0], ImageContainer);
+    }
+  }, 1000);
 };
 
 async function createSvg(img, container) {
@@ -25,9 +35,10 @@ async function createSvg(img, container) {
   svg.setAttribute("class", "svgContainer");
   svg.dataset.name = img.src.substring(0, img.src.length - 4);
   svg.innerHTML = svgText;
+  const addName = img.src.substring(0, img.src.length - 4);
   img.draggable
     ? createDraggableContainer(svg, parent, container, img)
-    : createNONContainer(parent);
+    : createNONContainer(parent, addName);
   parent.appendChild(svg);
   container.appendChild(parent);
   addAnimationToTheLampLid();
@@ -35,22 +46,27 @@ async function createSvg(img, container) {
 
 function createDraggableContainer(createdSvg, parent, container, img) {
   parent.classList.add("movableitemContainer");
+  parent.dataset.name = img.src.substring(0, img.src.length - 4);
   createdSvg.classList.add(`draggableItem`);
   createDropZone(img, container);
 }
 
 async function createDropZone(img, container) {
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const parent = document.createElement("div");
+  // const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   const responseSvg = await fetch(`images/level-images/${img.src}`);
   const svgText = await responseSvg.text();
-  svg.innerHTML = svgText;
-  svg.classList.add("dropzone");
-  svg.dataset.moving = "";
-  container.appendChild(svg);
+  parent.innerHTML = svgText;
+  parent.classList.add("dropzone");
+  parent.dataset.moving = "";
+  parent.dataset.name = img.src.substring(0, img.src.length - 4);
+  // parent.appendChild(svg);
+  container.appendChild(parent);
 }
 
-function createNONContainer(parent) {
+function createNONContainer(parent, addName) {
   parent.classList.add("NOTmovableitemContainer");
+  parent.dataset.name = addName;
 }
 
 function addAnimationToTheLampLid() {
--- modules/appendText.js
+++ modules/appendText.js
@@ -1,6 +1,25 @@
+import { gsap } from "gsap";
 export let AppendText = function(data) {
   const upperText = document.querySelector(".UpperText");
   const bottomText = document.querySelector(".Instructions");
-  upperText.textContent = data[0].textUpper;
-  bottomText.textContent = data[0].instruction;
+  gsap.to(upperText, {
+    duration: 1,
+    opacity: 0
+  });
+  gsap.to(bottomText, {
+    duration: 1,
+    opacity: 0
+  });
+  setTimeout(() => {
+    gsap.to(upperText, {
+      duration: 1,
+      opacity: 1
+    });
+    gsap.to(bottomText, {
+      duration: 1,
+      opacity: 1
+    });
+    upperText.textContent = data[0].textUpper;
+    bottomText.textContent = data[0].instruction;
+  }, 1000);
 };
